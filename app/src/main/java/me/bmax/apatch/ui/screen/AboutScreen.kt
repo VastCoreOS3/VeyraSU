68631456d58b81c65f1286490d441c96947add7d
VastCoreOS3 <zhangzhe250496549@qq.com>
2026-09-04 20:47:11 +0800

    Add files via upload

--- app/src/main/java/me/bmax/apatch/ui/screen/AboutScreen.kt
+++ app/src/main/java/me/bmax/apatch/ui/screen/AboutScreen.kt
@@ -3,13 +3,11 @@ package me.bmax.apatch.ui.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -19,7 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -29,7 +26,6 @@ import me.bmax.apatch.ui.screen.TabNavigator
 import me.bmax.apatch.BuildConfig
 import me.bmax.apatch.R
 import me.bmax.apatch.util.Version
-import top.yukonga.miuix.kmp.basic.Button
 import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.basic.IconButton
@@ -41,7 +37,6 @@ import top.yukonga.miuix.kmp.theme.MiuixTheme
 
 @Composable
 fun AboutScreen(navigator: TabNavigator) {
-    val uriHandler = LocalUriHandler.current
 
     Scaffold(
         topBar = {
@@ -92,39 +87,10 @@ fun AboutScreen(navigator: TabNavigator) {
                 modifier = Modifier.padding(top = 5.dp)
             )
 
-            Spacer(modifier = Modifier.height(20.dp))
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Button(
-                    onClick = { uriHandler.openUri("https://github.com/LyraVoid/FolkLite") }
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.github),
-                        contentDescription = null
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = stringResource(id = R.string.about_github))
-                }
-
-                Spacer(modifier = Modifier.width(10.dp))
-
-                Button(
-                    onClick = { uriHandler.openUri("https://t.me/FolkPatch") }
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.telegram),
-                        contentDescription = null
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = stringResource(id = R.string.about_telegram_channel))
-                }
-            }
+            Spacer(modifier = Modifier.height(30.dp))
 
             Card(
-                modifier = Modifier.padding(vertical = 30.dp, horizontal = 20.dp),
+                modifier = Modifier.padding(vertical = 10.dp, horizontal = 20.dp),
             ) {
                 Column(
                     modifier = Modifier
